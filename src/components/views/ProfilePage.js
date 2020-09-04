43d1080842ee45e4dcd667c01baafe4c23417a45
joelWarrington <joelwarrington@gmail.com>
2020-09-04 09:43:12 -0600

    started work on profile and rfi form

---
+++ src/components/views/ProfilePage.js
@@ -0,0 +1,44 @@
+import React from 'react';
+import { Container, Typography, Button, Paper, Grid } from '@material-ui/core';
+import { makeStyles, useTheme } from '@material-ui/styles';
+import { navigate } from '@reach/router';
+import Avatar, { Piece } from 'avataaars';
+import { withFirebase } from '../containers/FirebaseContext';
+
+const useStyles = makeStyles(theme => {
+  return {
+    root: {},
+    avatar: {
+      verticalAlign: 'middle',
+      margin: theme.spacing(2),
+      width: 200,
+      height: 200,
+      borderRadius: '50%',
+    },
+    paper: {
+      padding: theme.spacing(2),
+    },
+  };
+});
+
+const ProfilePage = props => {
+  const { firebase } = props;
+  const theme = useTheme();
+  const classes = useStyles(theme);
+  const { displayName, email, photoURL } = firebase.auth().currentUser;
+  return (
+    <Container maxWidth="md" className={classes.root}>
+      <br />
+      <Grid container direction="column" alignItems="center">
+        <Paper className={classes.paper}>
+          <Grid container direction="column" alignItems="center">
+            <img src={photoURL} className={classes.avatar} />
+            <Typography variant="h6">{displayName}</Typography>
+          </Grid>
+        </Paper>
+      </Grid>
+    </Container>
+  );
+};
+
+export default withFirebase(ProfilePage);
--- src/pages/profile.js
+++ src/pages/profile.js
@@ -1,6 +1,11 @@
 import React from 'react';
 import Layout from '../components/containers/Layout';
+import ProfilePage from '../components/views/ProfilePage';
 
-const IndexPage = () => <Layout></Layout>;
+const IndexPage = () => (
+  <Layout>
+    <ProfilePage />
+  </Layout>
+);
 
 export default IndexPage;
